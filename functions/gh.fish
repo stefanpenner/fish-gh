20ea600056192e6c273718547fe1133ea330cc93
Stefan Penner <stefan.penner@gmail.com>
2017-01-15 16:09:21 -0800

    cleanup

--- functions/gh.fish
+++ functions/gh.fish
@@ -1,14 +1,16 @@
 function gh -a fullname
   set name (string split '/' $fullname)
+
   set org $name[1]
   set repo $name[2]
-  set git git@github.com:$org/$repo
+
   set dest $HOME/src/$org/$repo
+
   mkdir -p $HOME/src/$org 2> /dev/null
-  if test -d $HOME/src/$org/$dest
-  else
-    echo "$git -> $dest"
-    git clone $git $dest
+
+  if not test -d "$dest"
+    git clone "git@github.com:$org/$repo" "$dest"
   end
+
   cd $dest
 end
